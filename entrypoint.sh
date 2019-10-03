a35cfc07c2a8d0d2c6183df58ca9b4c7e432e2ef
Yusuke Kuoka <ykuoka@gmail.com>
2019-10-04 08:57:14 +0900

    Add support for error reporting via issue comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,7 +33,14 @@ COMMENT="#### \`$VARIANT_NAME $*\` Failed
 $OUTPUT
 \`\`\`"
 PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
-COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
-curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null || true
+
+# Try to report the error either via a pull request comment or an issue comment
+COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url || true)
+if [ -z "$COMMENTS_URL" ]; then
+  COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .issue.comments_url || true)
+fi
+if [ -e "$COMMENTS_URL" ]; then
+  curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
+fi
 
 exit $SUCCESS
