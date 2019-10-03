a71659a2ff7253ddc5490f7a72960c086ffb0da5
Yusuke Kuoka <ykuoka@gmail.com>
2019-10-04 08:53:30 +0900

    Do not fail when comments_url unavailable

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,6 +34,6 @@ $OUTPUT
 \`\`\`"
 PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
 COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
-curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
+curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null || true
 
 exit $SUCCESS
