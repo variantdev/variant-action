b2418f8025e397266d373fbd97747ac5c36ab4d3
Yusuke Kuoka <ykuoka@gmail.com>
2019-10-05 00:33:33 +0900

    fix entrypoint.sh for correct report back functinality

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,10 +44,10 @@ PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
 
 # Try to report the error either via a pull request comment or an issue comment
 COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url || true)
-if [ -z "$COMMENTS_URL" ]; then
+if [ "$COMMENTS_URL" -eq "null" -o "$COMMENTS_URL" -ne "" ]; then
   COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .issue.comments_url || true)
 fi
-if [ -e "$COMMENTS_URL" ]; then
+if [ "$COMMENTS_URL" -ne "null" -a "$COMMENTS_URL" -ne "" ]; then
   curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
 fi
 
